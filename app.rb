49092ece6c4dee57252cb4d94469b1efe687c16d
Roman Pirag <mr.anadale@yahoo.com>
2018-12-11 23:47:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,29 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  # get '/reversename/:name' do 
+  #   reversed = []
+  #   string_split = params[:name].split("") 
+  #   string_split.length.times {|letter| reversed << letter.pop} 
+  #   reversed.join
+  # end
+  
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
+ 
+  get '/square/:number' do
+    (params[:number].to_i ** 2).to_s 
+  end
 
+  
+  get '/say/:number/:phrase' do
+    answer = ''
+
+    params[:number].to_i.times do
+      answer += params[:phrase]
+    end
+  end
+  
 end
